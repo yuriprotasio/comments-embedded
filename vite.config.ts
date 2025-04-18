0d4d7b1ba47a363e05a20f3fda49949f9bab599c
Yuri Protasio <yuriprotasio@hotmail.com>
2025-04-18 10:12:07 -0300

    fix types

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,5 +19,10 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    })
+  ],
 })
